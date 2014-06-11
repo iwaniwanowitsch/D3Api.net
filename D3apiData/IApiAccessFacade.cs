7051f892161e9327a17c99793ee422d71f31b8a9
Max Bk <iwaniwanowitsch1@web.de>
2014-06-11 11:21:58 +0200

    forgotten files

---
+++ D3apiData/IApiAccessFacade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D3apiData.API;
+using D3apiData.API.Objects;
+using D3apiData.Repositories;
+
+namespace D3apiData
+{
+    public interface IApiAccessFacade
+    {
+        CollectMode CollectMode { get; set; }
+
+        ProfileRepository ProfileRepository { get; }
+
+        HeroRepository HeroRepository { get; }
+
+        ItemRepository ItemRepository { get; }
+
+        FollowerRepository FollowerRepository { get; }
+
+        ArtisanRepository ArtisanRepository { get; }
+
+        SkillIconRepository SkillIconRepository { get; }
+
+        ItemIconRepository ItemIconRepository { get; }
+
+        IReadonlyRepository<Stream, string> ReadRepo { get; set; }
+
+    }
+}
