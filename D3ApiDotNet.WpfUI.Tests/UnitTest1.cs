0454f3e93b7eecdda8a611349541a86e189cca1a
Max Bk <iwaniwanowitsch1@web.de>
2014-06-12 21:37:36 +0200

    structure addition for wpf ui application

---
+++ D3ApiDotNet.WpfUI.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace D3ApiDotNet.WpfUI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
