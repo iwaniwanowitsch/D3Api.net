29a0b539442ca567944b2481085ef42a25c898ec
Max Bk <iwaniwanowitsch1@web.de>
2014-06-17 22:13:27 +0200

    hero info in load data view implemented

---
+++ D3ApiDotNet.WpfUI/Converters/BoolToHardcoreConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace D3ApiDotNet.WpfUI.Converters
+{
+    public class BoolToHardcoreConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var hardcore = value as bool?;
+            if (hardcore != null) {
+                if (targetType == typeof(Brush))
+                {
+                    var color = hardcore.Value ? Colors.Red : Colors.Yellow;
+                    return new SolidColorBrush(color);
+                }
+                return hardcore.Value ? "Hardcore" : "Softcore";
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
