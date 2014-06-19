7649f7e85c3ee0e543f074f7b2923080c07dddf1
lafaro <tobias.krueger2@gmail.com>
2014-06-19 14:24:37 +0200

    HeroClassIconConverter, Resources added

--- D3ApiDotNet.WpfUI/Converters/HeroClassIconConverter.cs
+++ D3ApiDotNet.WpfUI/Converters/HeroClassIconConverter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Globalization;
-using System.Windows;
+using System.IO;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 using D3ApiDotNet.Core.Objects.Hero;
@@ -9,11 +11,32 @@ namespace D3ApiDotNet.WpfUI.Converters
 {
     public class HeroClassIconConverter : IValueConverter
     {
+        private readonly Dictionary<HeroClass, Bitmap> _lookupDictionary = new Dictionary<HeroClass, Bitmap>
+        {
+            {HeroClass.Barbarian , Properties.Resources.BarbarianClassLogo32 },
+            {HeroClass.Crusader , Properties.Resources.logo32 },
+            {HeroClass.Demonhunter , Properties.Resources.logo32 },
+            {HeroClass.Monk , Properties.Resources.logo32 },
+            {HeroClass.Witchdoctor, Properties.Resources.WitchDoctorClassLogo32 },
+            {HeroClass.Wizard, Properties.Resources.WizardClassLogo32 }
+        };
+
+        private BitmapImage ToBitmapImage(Image image)
+        {
+            var memoryStream = new MemoryStream();
+            image.Save(memoryStream, image.RawFormat);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = memoryStream;
+            bitmapImage.EndInit();
+            return bitmapImage;
+        }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var heroClass = value is HeroClass ? ((HeroClass)value).ToString() : "Default";
-            var image = Application.Current.Resources["HeroClassImage" + heroClass] as BitmapImage;
-            return image;
+            var image = value is HeroClass ? _lookupDictionary[(HeroClass)value] : Properties.Resources.logo32;
+            return ToBitmapImage(image);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
